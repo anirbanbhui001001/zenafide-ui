039dbd6059e154f25d307e8d053c3043fff1deb0
digitalworkers9 <39660481-digitalworkers9@users.noreply.replit.com>
2025-02-14 11:02:02 +0000

    User checkpoint: Migrate project to Vite and HeroUI, updating project structure and styling.

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,6 +1,6 @@
-
+import DefaultLayout from "@/layouts/default";
 import Login from "@/components/Login";
 
 export default function LoginPage() {
-  return <Login />;
+  return <DefaultLayout>hello</DefaultLayout>;
 }
