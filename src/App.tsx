f2261671744730e242f40881a3d2f59679ea5b40
digitalworkers9 <39660481-digitalworkers9@users.noreply.replit.com>
2025-02-14 10:29:48 +0000

    User checkpoint: Fix: Correct route path for login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-
 import { Route, Routes } from "react-router-dom";
 import LoginPage from "@/pages/auth/login";
 
 function App() {
   return (
     <Routes>
-      <Route path="/auth/login" element={<LoginPage />} />
+      <Route path="/" element={<LoginPage />} />
     </Routes>
   );
 }
