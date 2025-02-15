68102bb51e2d437d3f3c9a3ff77210f50d005096
digitalworkers9 <39660481-digitalworkers9@users.noreply.replit.com>
2025-02-15 09:28:03 +0000

    User checkpoint: Refactor: Move user data to external file

--- src/components/sidebar/custom-sidebar-account-switcher.tsx
+++ src/components/sidebar/custom-sidebar-account-switcher.tsx
@@ -5,32 +5,7 @@ import React from "react";
 import {Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button, User, Avatar} from "@heroui/react";
 import {Icon} from "@iconify/react";
 
-const users = [
-  {
-    id: 1,
-    name: "Kate Moore",
-    role: "Customer Support",
-    team: "Customer Support",
-    avatar: "https://nextuipro.nyc3.cdn.digitaloceanspaces.com/components-images/avatars/e1b8ec120710c09589a12c0004f85825.jpg",
-    email: "kate.moore@example.com",
-  },
-  {
-    id: 2,
-    name: "John Doe",
-    role: "Product Designer",
-    team: "Design",
-    avatar: "https://i.pravatar.cc/150?u=a04258114e29026708c",
-    email: "john.doe@example.com",
-  },
-  {
-    id: 3,
-    name: "Jane Doe",
-    role: "Product Manager",
-    team: "Product",
-    avatar: "https://i.pravatar.cc/150?u=a04258114e22026708c",
-    email: "jane.doe@example.com",
-  },
-];
+import {users} from "@/data/user-dropdown";
 
 export default function AccountSwitcher() {
   return (
