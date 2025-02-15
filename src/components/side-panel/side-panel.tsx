b2e66f3fe7005f8fd64dcfe9ff016cdf03a453ae
digitalworkers9 <39660481-digitalworkers9@users.noreply.replit.com>
2025-02-15 12:20:41 +0000

    User checkpoint: Improve side-panel tab styling and layout.

--- src/components/side-panel/side-panel.tsx
+++ src/components/side-panel/side-panel.tsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import { Modal, ModalContent, ModalHeader, Tab, Tabs } from "@heroui/react";
 import SidePanelBasicDetails from "./side-panel-basic-details";
@@ -12,10 +11,15 @@ interface SidePanelProps {
   onSave: (user: ZenUser) => void;
 }
 
-export default function SidePanel({ isOpen, onClose, user, onSave }: SidePanelProps) {
+export default function SidePanel({
+  isOpen,
+  onClose,
+  user,
+  onSave,
+}: SidePanelProps) {
   return (
-    <Modal 
-      isOpen={isOpen} 
+    <Modal
+      isOpen={isOpen}
       onClose={onClose}
       placement="right"
       classNames={{
@@ -24,15 +28,25 @@ export default function SidePanel({ isOpen, onClose, user, onSave }: SidePanelPr
       }}
     >
       <ModalContent>
-        <ModalHeader className="flex flex-col gap-1">
-          {user ? `Edit ${user.firstName} ${user.lastName} (User id: ${user.id})` : "Add New User"}
+        <ModalHeader className="flex justify-between items-center  px-4 py-2 border-b">
+          {user
+            ? `Edit ${user.firstName} ${user.lastName} (User id: ${user.id})`
+            : "Add New User"}
         </ModalHeader>
         <Tabs aria-label="User details">
           <Tab key="basic-details" title="Basic Details">
-            <SidePanelBasicDetails user={user} onSave={onSave} onCancel={onClose} />
+            <SidePanelBasicDetails
+              user={user}
+              onSave={onSave}
+              onCancel={onClose}
+            />
           </Tab>
           <Tab key="permissions" title="Permissions">
-            <SidePanelPermissions user={user} onSave={onSave} onCancel={onClose} />
+            <SidePanelPermissions
+              user={user}
+              onSave={onSave}
+              onCancel={onClose}
+            />
           </Tab>
         </Tabs>
       </ModalContent>
