d4207a2a44439fdc2508ce291f7763c24431e988
digitalworkers9 <39660481-digitalworkers9@users.noreply.replit.com>
2025-02-17 15:00:28 +0000

    User checkpoint: Refactor: Move data files to improve organization of `src/data` directory.

--- src/pages/regulations/tabs/assistant-tab.tsx
+++
@@ -1,8 +0,0 @@
-
-import React from "react";
-import AssistantDetails from "@/components/assistant/assistant-details";
-import { chats } from "@/data/chats";
-
-export default function AssistantTab() {
-  return <AssistantDetails chats={chats} />;
-}
